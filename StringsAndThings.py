3ec3c338d78db39be6c180adbc5e98d26c2555e6
IzzieFedynich <54276173+IzzieFedynich@users.noreply.github.com>
2019-10-24 08:49:04 -0500

    Update StringsAndThings.py

--- StringsAndThings.py
+++ StringsAndThings.py
@@ -23,3 +23,21 @@ def rowYourBoat():
     print("Life is but a dream")
 
 rowYourBoat()
+
+# Indexing
+
+# Indexing gives us the ability to access any individual thing in a string
+#   like to ask a string how long it is
+
+name = "Roy G Biv"
+FirstChar = name[0]
+print(FirstChar)
+middleIndex = len(name) // 2
+# len is how you find out the length
+print(middleIndex)
+print(name[middleIndex])
+
+print(name[-1])
+
+for i in range(len(name)):
+    print(name[i])
