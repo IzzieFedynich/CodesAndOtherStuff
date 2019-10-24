8482f886b70158d64ac79665c0b9450018a56708
IzzieFedynich <54276173+IzzieFedynich@users.noreply.github.com>
2019-10-24 08:35:41 -0500

    Update StringsAndThings.py

--- StringsAndThings.py
+++ StringsAndThings.py
@@ -1,2 +1,25 @@
 # Strings
-#   data that falls within ""
+#   data that falls within "" marks
+
+# Concatenation
+#  Put 2 or more strings together
+
+firstName = "Fred"
+LastName = "Flintstone"
+
+fullName = firstName + " " + LastName
+
+print(fullName)
+
+# Repetition
+#   repetition operator: *
+
+print("Hip" *2 + "Horray!")
+
+def rowYourBoat():
+    print("Row, "*3 + ' your boat')
+    print("Gently down the stream")
+    print("Merrily, "*4)
+    print("Life is but a dream")
+
+rowYourBoat()
